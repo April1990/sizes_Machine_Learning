2fcd2882fed8c592f1ffdea5a17f90824e2e335a
April <aprilrose.veloro@student.kyh>
2022-05-17 13:22:34 +0200

    change for the file_modifier

--- csv/file_modifier.py
+++ csv/file_modifier.py
@@ -9,7 +9,7 @@ def female_top_clothing():
                  "chestbreadth", "chestcircumference", "chestdepth", "chestheight", "cervicaleheight", "neckcircumference",
                  "neckcircumferencebase", "suprasternaleheight", "Heightin", "weightkg", "Gender"]
 
-    f_tshirts_modified = df_female.to_csv('csv/modified_female_tshirts.csv', columns=f_tshirts, encoding='utf-8', index=False)
+    f_tshirts_modified = df_female.to_csv('modified_female_tshirts.csv', columns=f_tshirts, encoding='utf-8', index=False)
     return f_tshirts_modified
 
 
@@ -17,7 +17,7 @@ def female_bottom_clothing():
     f_shorts = ["iliocristaleheight", "kneeheightmidpatella", "kneeheightsitting", "lowerthighcircumference",
                 "poplitealheight", "thighcircumference", "thighclearance", "tibialheight", "waistheightomphalion", "Heightin", "weightkg", "Gender"]
 
-    f_shorts_modified = df_female.to_csv('csv/modified_female_shorts.csv', columns=f_shorts, encoding='utf-8', index=False)
+    f_shorts_modified = df_female.to_csv('modified_female_shorts.csv', columns=f_shorts, encoding='utf-8', index=False)
     return f_shorts_modified
 
 
@@ -26,7 +26,7 @@ def male_top_clothing():
                  "chestbreadth", "chestcircumference", "chestdepth", "chestheight", "cervicaleheight", "neckcircumference",
                  "neckcircumferencebase", "suprasternaleheight", "Heightin", "weightkg", "Gender"]
 
-    m_tshirts_modified = df_male.to_csv('csv/modified_male_tshirts.csv', columns=m_tshirts, encoding='utf-8', index=False)
+    m_tshirts_modified = df_male.to_csv('modified_male_tshirts.csv', columns=m_tshirts, encoding='utf-8', index=False)
     return m_tshirts_modified
 
 
@@ -34,7 +34,7 @@ def male_bottom_clothing():
     m_shorts = ["iliocristaleheight", "kneeheightmidpatella", "kneeheightsitting", "lowerthighcircumference",
                 "poplitealheight", "thighcircumference", "thighclearance", "tibialheight", "waistheightomphalion", "Heightin", "weightkg", "Gender"]
 
-    m_shorts_modified = df_male.to_csv('csv/modified_male_shorts.csv', columns=m_shorts, encoding='utf-8', index=False)
+    m_shorts_modified = df_male.to_csv('modified_male_shorts.csv', columns=m_shorts, encoding='utf-8', index=False)
     return m_shorts_modified
 
 
@@ -45,5 +45,5 @@ if __name__ == '__main__':
     male_bottom_clothing()
 
 ''''
-df_female = pd.read_csv('csv/female.csv' usecols=f_tshirts, index_col=None, encoding='utf-8')
+df_female = pd.read_csv('female.csv' usecols=f_tshirts, index_col=None, encoding='utf-8')
 '''
