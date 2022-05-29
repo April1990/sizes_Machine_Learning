194ad877d3662ce5963df88ee4d824adddbc3e81
April <aprilrose.veloro@student.kyh>
2022-05-29 09:36:15 +0200

    change for male csv

--- csv/sample.py
+++ csv/sample.py
@@ -5,14 +5,14 @@ df_male = pd.read_csv('male.csv')
 size = []
 
 
-def female_top_clothing():
-    df_female['Heightin'] = df_female['Heightin'].multiply(2.54)
-    df_female['Weightlbs'] = df_female['Weightlbs'].multiply(0.45359237)
-    df_female['chestcircumference'] = df_female['chestcircumference'].multiply(0.1)
-    df_female['waistcircumference'] = df_female['waistcircumference'].multiply(0.1)
-    df_female.rename(columns={"chestcircumference": "chest", "waistcircumference": "waist", "Heightin": "height", "Weightlbs": "weight", "Gender": "gender"}, inplace=True)
+def top_clothing(read_file, write_file):
+    read_file['Heightin'] = read_file['Heightin'].multiply(2.54)
+    read_file['Weightlbs'] = read_file['Weightlbs'].multiply(0.45359237)
+    read_file['chestcircumference'] = read_file['chestcircumference'].multiply(0.1)
+    read_file['waistcircumference'] = read_file['waistcircumference'].multiply(0.1)
+    read_file.rename(columns={"chestcircumference": "chest", "waistcircumference": "waist", "Heightin": "height", "Weightlbs": "weight", "Gender": "gender"}, inplace=True)
 
-    for chest, waist in zip(df_female['chest'], df_female['waist']):
+    for chest, waist in zip(read_file['chest'], read_file['waist']):
         if chest <= 81:
             if waist <= 76:
                 size.append('XS')
@@ -41,20 +41,15 @@ def female_top_clothing():
             else:
                 size.append('XXL')
 
-
-    df_female['size'] = pd.Series(size)
+    read_file['size'] = pd.Series(size)
     f_tshirt = ['chest', 'waist', 'height', 'weight', 'size', 'gender']
-    modified_female_tshirt = df_female.to_csv('modified_female_tshirts.csv', columns=f_tshirt, encoding='utf-8', index=False)
+    modified_female_tshirt = read_file.to_csv(write_file, columns=f_tshirt, encoding='utf-8', index=False)
     return modified_female_tshirt
 
 
 if __name__ == '__main__':
-    female_top_clothing()
+    top_clothing(read_file=df_female, write_file='modified_female_tshirts.csv')
+    top_clothing(read_file=df_male, write_file='modified_male_tshirts.csv')
 
-'''
-        if 81 < chest < 85:
-            if 65 < waist < 68:
-                size.append('S')
-            else:
-                size.append('XS')
-'''
+
+
